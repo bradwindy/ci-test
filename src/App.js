c4e05c32c0d6322c51f780dc17dd6e460cf5a548
Bradley Windybank <trykryda@gmail.com>
2019-07-17 14:45:39 +1200

    CI Test commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <h1>This is a CI Test!</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <button>Show</button>
+            </div>
+        );
+    }
 }
-
 export default App;
