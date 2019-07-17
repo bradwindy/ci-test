3126e2e9feddea738c203bb57989621d9f33f870
Bradley Windybank <trykryda@gmail.com>
2019-07-17 14:54:50 +1200

    Added and updated tests.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import App from '../App';
+import ReactDOM from 'react-dom';
 import {create} from 'react-test-renderer'
 
-describe('My first snapshot test', () => {
-    test('testing app button', () => {
+describe('App Button Match', () => {
+    test('Testing app button', () => {
         let tree = create(<App/>);
         expect(tree.toJSON()).toMatchSnapshot();
     })
 });
+
+describe('Render Test', () => {
+    it('Render without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App/>, div);
+    });
+});
+
+
